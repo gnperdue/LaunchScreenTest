076fc3154291fe0eb4cc1eea03629c280cc23917
Gabriel Perdue <gnperdue@hey.com>
2021-01-09 15:53:29 -0600

    Initial Commit

---
+++ LaunchScreenTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LaunchScreenTest
+//
+//  Created by Gabriel Perdue on 1/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LaunchScreenTest/LaunchScreenTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchScreenTestApp.swift
+//  LaunchScreenTest
+//
+//  Created by Gabriel Perdue on 1/9/21.
+//
+
+import SwiftUI
+
+@main
+struct LaunchScreenTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
